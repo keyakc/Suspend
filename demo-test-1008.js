8d229e8543f31f9ba37f9cb94aedd67fcec1fe35
wakaCon <wgs.animation@163.com>
2017-11-17 23:48:05 +0800

    1117

--- demo-test-1008.js
+++ demo-test-1008.js
@@ -1,34 +1,33 @@
-var banner=document.getElementById("banner");
-var nodesOfBanners=banner.getElementsByTagName("*");
+var $banner=$(".mod_banner");
+var $banner_children=$banner.children();
 var inFlag,offsetX,offsetY;
-banner.onmouseenter=function(event) {
-	event=event||window.event;
+$banner.mouseenter(function(event) {
 	inFlag=1;
 	offsetX=event.offsetX;
 	offsetY=event.offsetY;
-}
-banner.onmouseleave=function() {
+})
+$banner.mouseleave(function() {
 	inFlag=0;
-	for(i=0;i<nodesOfBanners.length;i++){
-	nodesOfBanners[i].setAttribute("style",strGener(0,0,0,0));
+	for(i=0;i<$banner_children.length;i++){
+	$banner_children.eq(i).attr("style",strGener(0,0,0,0));
 	}
-}
-banner.onmousemove=function(event) {
+})
+$banner.mousemove(function(event) {
 	if(inFlag!=1) return;
 	event=event||window.event;
 	var newOffsetX=parseInt(event.offsetX),
 		newOffsetY=parseInt(event.offsetY),
-	 	width=parseInt(window.getComputedStyle(banner,null).width),
-	    height=parseInt(window.getComputedStyle(banner,null).height),
+	 	width=$banner.outerWidth(),
+	    height=$banner.outerHeight(),
 	    degOffsetX=(height/2-newOffsetY)*20/height,
 	    degOffsetY=(newOffsetX-width/2)*20/width,
-		str="transform:rotateX("+degOffsetX+"deg) rotateY("+degOffsetY+"deg);"		
-		nodesOfBanners[0].setAttribute("style",strGener(degOffsetX,degOffsetY,0,0));
-		nodesOfBanners[1].setAttribute("style",strGener(degOffsetX,degOffsetY*2,degOffsetY,degOffsetX/(-1.5)));
-		nodesOfBanners[2].setAttribute("style",strGener(degOffsetX,degOffsetY*2,degOffsetY*(4),degOffsetX*(-3)));
-		nodesOfBanners[3].setAttribute("style",strGener(degOffsetX,degOffsetY,degOffsetY*2,degOffsetX*(-2)));
-		nodesOfBanners[4].setAttribute("style",strGener(degOffsetX,degOffsetY,degOffsetY*3,degOffsetX*(-3)));
-}
+		str="transform:rotateX("+degOffsetX+"deg) rotateY("+degOffsetY+"deg);";	
+		$banner_children.eq(0).attr("style",strGener(degOffsetX,degOffsetY,0,0));
+		$banner_children.eq(1).attr("style",strGener(degOffsetX,degOffsetY*2,degOffsetY,degOffsetX/(-1.5)));
+		$banner_children.eq(2).attr("style",strGener(degOffsetX,degOffsetY*2,degOffsetY*(4),degOffsetX*(-3)));
+		$banner_children.eq(3).attr("style",strGener(degOffsetX,degOffsetY,degOffsetY*2,degOffsetX*(-2)));
+		$banner_children.eq(4).attr("style",strGener(degOffsetX,degOffsetY,degOffsetY*3,degOffsetX*(-3)));
+})
 function strGener(degOffsetX,degOffsetY,translateX,translateY){
 	var str="transform:rotateX("+degOffsetX+"deg) rotateY("+degOffsetY+"deg) translate("+translateX+"px,"+translateY+"px);";
 	return str;
